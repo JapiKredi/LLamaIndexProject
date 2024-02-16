6b2a2aa4f2c765e06bb9c5bf060f2d5f122f05b9
JapiKredi <jasper.bongers@yahoo.com>
2024-02-16 16:28:34 +0800

    commit

--- webpagereader.py
+++ webpagereader.py
@@ -3,9 +3,9 @@ print(nltk.__version__)
 #nltk.data.path.append(os.path.expanduser('~/nltk_data'))
 #nltk.data.path.append('/Users/jasper/nltk_data')
 
-#import llama_index
+import llama_index
 #from llama_index.llms.openai import OpenAI
-#from llama_index.core import Settings
+from llama_index.core import Settings
 #from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
 
 import os
