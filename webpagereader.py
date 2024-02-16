b8412a83bd91d61e71bd30e20eda8fed5052e036
JapiKredi <jasper.bongers@yahoo.com>
2024-02-16 15:40:30 +0800

    import dependencies for webpagereader

--- webpagereader.py
+++ webpagereader.py
@@ -1,1 +1,6 @@
-from llama-index.llm import OpenAI
+import llama_index
+from llama_index.llm import OpenAI
+from llama_index.readers import SimpleWebPageReader
+from llama_index import VectorStoreIndex
+import os
+from dotenv import load_dotenv
