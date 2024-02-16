80ce0fcca201e0cba5a796b3427e068919f8366a
JapiKredi <jasper.bongers@yahoo.com>
2024-02-16 16:27:28 +0800

    de-bugging issue with nltk package while working with llama-index

--- webpagereader.py
+++ webpagereader.py
@@ -1,11 +1,12 @@
-import llama_index
-from llama_index.llms.openai import OpenAI
-from llama_index.core import Settings
-from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
+import nltk
+print(nltk.__version__)
+#nltk.data.path.append(os.path.expanduser('~/nltk_data'))
+#nltk.data.path.append('/Users/jasper/nltk_data')
+
+#import llama_index
+#from llama_index.llms.openai import OpenAI
+#from llama_index.core import Settings
+#from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
 
 import os
 from dotenv import load_dotenv
-import nltk
-
-#nltk.data.path.append(os.path.expanduser('~/nltk_data'))
-nltk.data.path.append('/Users/jasper/nltk_data')
